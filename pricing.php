c740792226189f2db699a72a5fc8c3da514f6dca
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2019-05-01 09:30:16 +0545

    aded first time

---
+++ pricing.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <title>ReceiveCo</title>
+    <link rel="stylesheet" href="pricing.css" >
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+  </head>
+<body>
+   
+    <!--navbar-->
+    <nav class="navbar navbar-expand-lg navbar-dark static-top fixed">
+      <div class="container">
+        <a class="navbar-brand" href="#">Review Pear</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">GET STARTED
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">PRICING</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">LOGIN</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">CONTACT</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!--navbar ends-->
+    <h1 id="main-header">Tool to help your business thrive<br>
+        in a volatile market </h1>
+    <div class="demo">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-6">
+                    <div class="pricingTable">
+                        <div class="pricingTable-header">
+                            <h3 class="title">Starting</h3>
+                        </div>
+                        <div class="price-value">
+                            <span class="amount">39</span>
+                            <span class="amount-sm">.00</span>
+                            <span class="currency">$/mo</span>
+                        </div>
+                        <ul class="pricing-content">
+                            <li><i class="fas fa-check"></i> Unlimited emails</li>
+                            <li><i class="fas fa-check"></i> 200 SMS invites</li>
+                            <li><i class="fas fa-check"></i> Custom Invite Templates</li>
+                            <li><i class="fas fa-check"></i> Review Reminders</li>
+                            <li><i class="fas fa-check"></i> 20 Domains</li>
+                        </ul>
+                        <a href="#" class="pricingTable-signup">Start 14 day Trial</a>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6">
+                    <div class="pricingTable red">
+                        <div class="pricingTable-header">
+                            <h3 class="title">Growing</h3>
+                        </div>
+                        <div class="price-value">
+                            <span class="amount">69</span>
+                            <span class="amount-sm">.00</span>
+                            <span class="currency">$/mo</span>
+                        </div>
+                        <ul class="pricing-content">
+                            <li><i class="fas fa-check"></i> Unlimited emails</li>
+                            <li><i class="fas fa-check"></i> 200 SMS invites</li>
+                            <li><i class="fas fa-check"></i> Custom Invite Templates</li>
+                            <li><i class="fas fa-check"></i> Review Reminders</li>
+                            <li><i class="fas fa-check"></i> 20 Domains</li>
+                        </ul>
+                        <a href="#" class="pricingTable-signup">Start 14 day Trial</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
